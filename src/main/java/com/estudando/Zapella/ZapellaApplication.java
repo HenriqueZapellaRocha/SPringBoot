33359869a2a144fcfb14736f5e7f2f16dbdd5dd8
HenriqueZapellaRocha <henri_Zap@Hotmail.com>
2024-04-10 11:44:46 -0300

    first commit

---
+++ src/main/java/com/estudando/Zapella/ZapellaApplication.java
@@ -0,0 +1,13 @@
+package com.estudando.Zapella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZapellaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZapellaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudando/Zapella/ZapellaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudando.Zapella;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZapellaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
