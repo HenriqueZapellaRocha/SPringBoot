ea5101633f84436410e78fff02d2c20466c4dde0
HenriqueZapellaRocha <henri_Zap@Hotmail.com>
2024-04-10 12:45:08 -0300

    adicionado jpa h2 user

---
+++ src/main/java/com/estudando/Zapella/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.estudando.Zapella.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.estudando.Zapella.entitites.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+    
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1l, " maria", " mariagmail"," asdad", " sadsadada");
+        return ResponseEntity.ok().body(u);
+    }
+}
